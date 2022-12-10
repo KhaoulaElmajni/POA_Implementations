9948ffa19f6a23870e551d7ba51e128eb3ccea5d
Khaoula Elmajni <elmajnikhaoula99@gmail.com>
2022-12-10 19:55:16 +0100

    delete

--- TP-AOP/src/main/java/metier/IMetierBanque.java
+++
@@ -1,8 +0,0 @@
-package metier;
-
-public interface IMetierBanque {
-     void addCompt(Compte cp);
-    void verser(Long code,double montant);
-    void retirer(Long code,double montant);
-    Compte consulter(Long code);
-}
